5b506cacf77f93caebcf196976cb9f6707db703e
Reven Sanchez <reven@reven.org>
2010-02-28 05:49:59 +0100

    Textinterpreter listo, pero no implementado

--- engine/textinterpreter.php
+++ engine/textinterpreter.php
@@ -1,35 +1,22 @@
 <?php
 
 /*
-Vale,
-
-Después de jugar un poco con el código, no estoy seguro de que quiera esto. Es decir, para tener que poner *sdad* más me vale poner <b>kjhkj</b>,
-no es tanto ahorro de teclear. O podemos dejar unas básicas (como *, _ y las listas), pero para lo demás me parece mejor hacer html puro y duro, que
-esto sólo sea un atajo. Mmm... no sé.
-
-El problema adicional es que como queremos que la edición sea in situ, el texto de la base de datos debe ser html. De lo contrario, si es texto
-con símbolos, se traduce a html para que se visualice correctamente en la página y al editarlo, se está editando el html... Debemos cambiar a html
-a la hora de guardar, por lo que no haría falta para visualizar, sólo para guardar.
-
-Pero entonces lo que pasa es que como los regexp están definidos a lo bruto (en vez de detectar las listas con '*' hacemos dos pasos intermedios y en 
-el último cambiamos los <li>), entonces pillan también el html que ya está limpio. Para evitarlo habría que pasar todo a símbolos, editar, convertir
-a html y guardar. Impráctico, porque entonces jode el html.
-
-Creo que de momento dejo estas funciones para más adelante.
-
 ______________________________________________________________________________________
-Básicamente queremos implementar el lenguaje de las Writeboards:
-                    
+Text-interpreter (mini-markdown) by Reven ©2010
+--------------------------------------------------------------------------------------
+
+Sustituciones:
+
 *sdfsfdsdf*         -> negritas
 _jhkhkjhkk_         -> cursiva
-                    
-*Item               -> lista
-*Item               
-                    
+
+* Item              -> lista
+* Item              
+
 1. Item             -> lista numerada
 2. Item             
-                    
-                    
+
+
 bq. Indented block  -> Bloque indentado
 
 h. Titulo           -> Cabecera (h3, porque h1 es el logotipo y h2 es el título de la página, aunque se puede depurar el css)
@@ -38,24 +25,68 @@ h. Titulo           -> Cabecera (h3, porque h1 es el logotipo y h2 es el título
 
 !http://37signals.com/logo.gif!   -> imagen
 
-Podemos empezar por los sencillos (negritas, cursivas y listas)
 */
 
-function get_html($input){
-	$text = preg_replace("/(.+)\r\n\r\n/i","<p>$1</p>\n",$input);
-	$text = preg_replace("/\*(.*)\*/i","<b>$1</b>",$text);
-	$text = preg_replace("/_(.*)_/ig","<i>$1</i>",$text);
-	
-	//listas. bufff!!!
-	$text = preg_replace("/\*+(.*)?/i","<li>$1</li>",$text);
-	$text = preg_replace("/(\<li\>)(.*)\s(\<\/li\>)/i","$1$2$3",$text); //horrible. Para limpiar algunos li
-	$text = preg_replace("/(\<li\>(.*)\<\/li\>\n*)+/is","<ul>\n$1\n</ul>\n",$text); //Igualmente horrible. Tiene que haber una forma más elegante!!!
-	//Problema: las tres lineas anteriores se tienen que hacer en un mismo paso, para que no interfiera con la detección de las listas ordenadas.
-	//Lo dejo por ahora
-	//$text = preg_replace("/\d+(.*)?/i","<li>$1</li>",$text);
-	//$text = preg_replace("/(\<li\>)(.*)\s(\<\/li\>)/i","$1$2$3",$text); 
-	//$text = preg_replace("/(\<li\>(.*)\<\/li\>\n*)+/is","<ol>\n$1\n</ol>\n",$text); 
+function get_html($input){ 
+	# Standardize line endings: DOS to Unix and Mac to Unix
+	$text = preg_replace('{\r\n?}', "\n", $input);
 
+	$regex = array(
+		'/\*(.*?)\*/',
+		'/_(.*?)_/',
+		'/^h\.\s?(.*)$/m',
+		'@"(.+)":(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)@', // Enlaces con texto: "texto":http://www.37signals.com
+		'@!(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)!@', //imágen
+
+	);
+	$replace = array(
+		"<strong>$1</strong>",
+		"<em>$1</em>",
+		"<h3>$1</h3>",
+		"<a href=\"$2\">$1</a>",
+		"<img src=\"$1\" />",
+	);
+	
+	$text = preg_replace($regex,$replace,$text);
+
+	$paragraphs = explode("\n\n", $text);
+	//echo '<div style="color:#f00; border: 1px solid #f00;">'. print_r ($paragraphs, TRUE)."</div>";
+	$text = null;
+    foreach($paragraphs as $paragraph) {
+		if (!preg_match("/^(\n?)[<|\*|\d|bq\.]/",$paragraph)){
+			$paragraph = preg_replace('/(.+)\n(.+)/',"$1<br />$2",$paragraph);
+			$text .= "\n<p>".$paragraph."</p>\n";
+			
+		}else{
+			$paragraph = preg_replace("/^(\t?)\*+\s?(.*)/m","$1<uli>$2</uli>",$paragraph);
+			$paragraph = preg_replace("/(\<uli\>(.+?)\<\/uli\>)$/s","<ul>\n$1\n</ul>",$paragraph);
+			$paragraph = preg_replace("/^(\t?)\d+\.+\s?(.*)/m","$1<oli>$2</oli>",$paragraph);
+			$paragraph = preg_replace("/(\<oli\>(.+?)\<\/oli\>)$/s","<ol>\n$1\n</ol>",$paragraph);
+			//Sublistas ---> Solo un nivel de recursión
+			//intentar arreglar sublistas --> ul dentro de ul
+			$paragraph = preg_replace("#(\</uli\>\n\t\<uli\>)((.+?\</uli\>)\n)(^\<uli\>)#ms","\n\t<ul><li>$3</ul></li>\n<li>",$paragraph);
+			//intentar arreglar sublistas --> ol dentro de ol
+			$paragraph = preg_replace("#(\</oli\>\n\t\<oli\>)((.+?\</oli\>)\n)(^\<oli\>)#ms","\n\t<ul><li>$3</ul></li>\n<li>",$paragraph);
+			//intentar arreglar sublistas --> ol dentro de ul
+			$paragraph = preg_replace("#(\</uli\>\n\t\<oli\>)((.+?\</oli\>)\n)(^\<uli\>)#ms","\n\t<ol><li>$3</ol></li>\n<li>",$paragraph);
+			//intentar arreglar sublistas --> ul dentro de ol
+			$paragraph = preg_replace("#(\</oli\>\n\t\<uli\>)((.+?\</uli\>)\n)(^\<oli\>)#ms","\n\t<ul><li>$3</ul></li>\n<li>",$paragraph);
+			
+			//Blockquotes anidados
+			while (preg_match("/bq\./",$paragraph)){
+				$paragraph = preg_replace('/bq\.\s(.*)/sm', "<blockquote>$1</blockquote>\n",$paragraph);
+			}
+			
+			$text .= "\n".$paragraph."\n";
+			
+		}
+    }
+	$text = preg_replace("/\<uli\>/","<li>",$text);
+	$text = preg_replace("/\<\/uli\>/","</li>",$text);
+	$text = preg_replace("/\<oli\>/","<li>",$text);
+	$text = preg_replace("/\<\/oli\>/","</li>",$text);
 	return ($text);
 }
+
+
 ?>
